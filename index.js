5b42c3e50c9d49c78545464ea89f0f0e7a7d64f1
Benta <bentaakitwi500@gmail.com>
2023-03-15 22:43:52 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+function saturdayFun (toDO = "roller-skate" ){
+    return `This Saturday, I want to ${toDO}!`;
+}
+saturdayFun();
+saturdayFun("bathe my dog");
+
+function mondayWork (day = "go to the office" ){
+    return `This Monday, I will ${day}.`;
+}
+mondayWork();
+mondayWork("work from home");
+function wrapAdjective(testing = "*" ){
+   return function (mypar = "special"){
+    return `You are ${testing}${mypar}${testing}!`;
+   }
+    }
+wrapAdjective()("a hard worker");
+wrapAdjective("||",)("a dedicated programmer");
